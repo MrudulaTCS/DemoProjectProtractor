a7d4c7d7078e4b70507cb8cdaf7f61c0d0c51d05
Velugubantla <mrudula.velugubantla@bt.com>
2018-09-26 10:07:37 +0100

    test

--- sample-spec.js
+++ sample-spec.js
@@ -1,31 +1,41 @@
 var EC = protractor.ExpectedConditions;
 
+
+// describe('Navigation of Login Button', function() {
+//   it('Should launch the application in the browser', function() {
+//     browser.get('https://dev.tvac.bt.com/web/prod');
+//     browser.manage().window().maximize();
+//     var testElement = element(by.css('#login_title_id'));
+//   });
+
+
+
+//  ------ Sample with Google-------
+
 describe('Navigation of Login Button', function() {
   it('Should launch the application in the browser', function() {
-    //browser.get('https://sport:sport@dev.tvac.bt.com/sportApp/');
-    browser.get('https://www.google.com');
+    browser.get('https://sport:sport@dev.tvac.bt.com/sportApp/');
+    //browser.get('https://www.google.com');
     browser.manage().window().maximize();
     browser.executeScript("document.body.style.zoom='67%'");
-    var testElement = element(by.css('#login_title_id'));
-    browser.wait(expect(testElement.isPresent()).toBeTruthy(), 10000);
-    //expect(testElement.getText()).toEqual('Enter your BT ID');
+    var loginButton = element(by.css('#login_button_id'));
+    browser.wait(EC.visibilityOf(loginButton), 15000);
   });
-
-  // it('Should see the Login button', function() {
-  //   var loginButton = element(by.css('#login_button_id'));
-  //   //browser.wait(EC.visibilityOf(loginButton), 15000);
-  //   //expect(loginButton.isPresent()).toEqual(true);
-  //   browser.wait(expect(loginButton.isPresent()).toBeTruthy(), 10000);
-  //   browser.sleep(3000);
-  // });
   //
-  // it('Should able to see the Enter BT ID page', function() {
-  //   browser.actions().sendKeys(protractor.Key.ENTER).perform();
-  //   browser.actions().sendKeys(protractor.Key.RIGHT).perform();
-  //   var testElement = element(by.css('#login_title_id'));
-  //   browser.wait(expect(testElement.isPresent()).toBeTruthy(), 10000);
-  //   expect(testElement.getText()).toEqual('Enter your BT ID');
-  //   browser.sleep(3000);
-  // });
-
+  //   // it('Should see the Login button', function() {
+  //   //   var loginButton = element(by.css('#login_button_id'));
+  //   //   //browser.wait(EC.visibilityOf(loginButton), 15000);
+  //   //   //expect(loginButton.isPresent()).toEqual(true);
+  //   //   browser.wait(expect(loginButton.isPresent()).toBeTruthy(), 10000);
+  //   //   browser.sleep(3000);
+  //   // });
+  //   //
+  //   // it('Should able to see the Enter BT ID page', function() {
+  //   //   browser.actions().sendKeys(protractor.Key.ENTER).perform();
+  //   //   browser.actions().sendKeys(protractor.Key.RIGHT).perform();
+  //   //   var testElement = element(by.css('#login_title_id'));
+  //   //   browser.wait(expect(testElement.isPresent()).toBeTruthy(), 10000);
+  //   //   expect(testElement.getText()).toEqual('Enter your BT ID');
+  //   //   browser.sleep(3000);
+  //   // });
 });

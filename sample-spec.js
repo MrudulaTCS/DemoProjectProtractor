108a17a7da1bc15a1ba1694131c862630005fa74
Velugubantla <mrudula.velugubantla@bt.com>
2018-09-26 14:33:04 +0100

    test

--- sample-spec.js
+++ sample-spec.js
@@ -37,12 +37,12 @@ describe('Navigation of Login Button', function() {
     // browser.sleep(3000);
   });
   //   //
-  //   // it('Should able to see the Enter BT ID page', function() {
-  //   //   browser.actions().sendKeys(protractor.Key.ENTER).perform();
-  //   //   browser.actions().sendKeys(protractor.Key.RIGHT).perform();
-  //   //   var testElement = element(by.css('#login_title_id'));
-  //   //   browser.wait(expect(testElement.isPresent()).toBeTruthy(), 10000);
-  //   //   expect(testElement.getText()).toEqual('Enter your BT ID');
-  //   //   browser.sleep(3000);
-  //   // });
+  it('Should able to see the Enter BT ID page', function() {
+    browser.actions().sendKeys(protractor.Key.ENTER).perform();
+    browser.actions().sendKeys(protractor.Key.RIGHT).perform();
+    var testElement = element(by.css('#login_title_id'));
+    browser.wait(expect(testElement.isPresent()).toBeTruthy(), 10000);
+    expect(testElement.getText()).toEqual('Enter your BT ID');
+    browser.sleep(3000);
+  });
 });

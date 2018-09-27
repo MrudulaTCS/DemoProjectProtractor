a77dd6a6487bae833853cdd4a427d98e50ebfb73
Velugubantla <mrudula.velugubantla@bt.com>
2018-09-27 15:22:45 +0100

    test

--- conf-html-reporter.js
+++ conf-html-reporter.js
@@ -6,7 +6,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ["--headless", "--window-size=800x600"]
+      args: ["--headless", "--disable-gpu", "--window-size=800x600"]
     }
   },
   jasmineNodeOpts: {

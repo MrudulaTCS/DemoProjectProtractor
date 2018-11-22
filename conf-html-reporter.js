a848241253f2c1d9508fd9f62f8a81a89d50a99d
MrudulaTCS <32129383+MrudulaTCS@users.noreply.github.com>
2018-11-22 18:54:44 +0000

    Update conf-html-reporter.js

--- conf-html-reporter.js
+++ conf-html-reporter.js
@@ -1,5 +1,5 @@
 exports.config = {
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['upsell_test.js'],
   framework: 'jasmine2',
   //allScriptsTimeout: 600000,
